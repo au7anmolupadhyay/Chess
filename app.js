cf20c2d76c558114414c487b9c5bb847c75d6247
Anmol Upadhyay <anmolupadhau7@gmail.com>
2025-05-24 20:50:48 +0530

    added game logic unicodes and board

--- app.js
+++ app.js
@@ -21,11 +21,56 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (uniqueSocket) => {
-    console.log('New connection', uniqueSocket.id);
+    console.log('User connected', uniqueSocket.id);
 
-    uniqueSocket.on("Football", function(){
-        console.log("football game is going to start");
-    })
+    if(!players.white){
+        players.white = uniqueSocket.id;
+        uniqueSocket.emit("playerRole", "w");
+    }
+    else if(!players.black){
+        players.black = uniqueSocket.id;
+        uniqueSocket.emit("playerRole", "b");
+    }
+    else{
+        uniqueSocket.emit("spectatorRole");
+    }
+
+    uniqueSocket.on("disconnect", function(){
+        if(uniqueSocket.id === players.white){
+            delete players.white;
+        }
+        else if(uniqueSocket.id === players.black){
+            delete players.black;
+        }
+    });
+
+    uniqueSocket.on("move", (move)=>{
+        try{
+            if(chess.turn()==="w" && uniqueSocket.id !== players.white){
+                uniqueSocket.emit("error", "It's not your turn");
+                return;
+            }
+            else if(chess.turn()==="b" && uniqueSocket.id !== players.black){
+                uniqueSocket.emit("error", "It's not your turn");
+                return;
+            }
+
+            const result = chess.move(move);
+            if(result){
+                currentPlayer = chess.turn();
+                io.emit("move", move); // io.emit means sbko broadcast krega
+                io.emit("boardState", chess.fen())
+            }
+            else{
+                console.log("Invald move: ", move);
+                uniqueSocket.emit("Invalid move: ", move);  // here why uniqueSocket.emit? because we want to send error to the user who made the move, not to all the users.
+            }
+        }
+        catch(err){
+            console.log("Error: ", err);
+            uniqueSocket.emit("Error, Invalid move: ", move);
+        }
+    });
 });
     
 
--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,3 +1,92 @@
-const socket = io();
+const socket = io(); // connection event fire automatically for backend when server is running
+const chess = new Chess();
 
-socket.emit("Football");
+const boardElement = document.querySelector(".chessboard");
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderBoard = () => {
+  const board = chess.board();
+  boardElement.innerHTML = ""; //clear the board
+
+  board.forEach((row, rowIndex) => {
+    row.forEach((square, squareIndex) => {
+      const squareElement = document.createElement("div");
+      squareElement.classList.add(
+        "square",
+        (rowIndex + squareIndex) % 2 === 0 ? "light" : "dark"
+      );
+
+      squareElement.dataset.row = rowIndex;
+      squareElement.dataset.col = squareIndex;
+
+      if (square) {
+        const pieceElement = document.createElement("div");
+        pieceElement.classList.add(
+          "piece",
+          square.color === "w" ? "white" : "black",
+          square.type
+        );
+        pieceElement.innerText = getPieceUnicode(square);
+        pieceElement.draggable = playerRole === square.color;
+
+        pieceElement.addEventListener("dragstart", (e) => {
+          if (pieceElement.draggable) {
+            draggedPiece = pieceElement;
+            sourceSquare = {row: rowIndex, col: squareIndex};
+            e.dataTransfer.setData("text/plain", ""); // to prevent any default behavior on cross platforms
+          }
+        });
+
+        pieceElement.addEventListener("dragend", (e) => {
+            draggedPiece = null;
+            sourceSquare = null;
+        });
+
+        squareElement.appendChild(pieceElement);
+      }
+
+      squareElement.addEventListener("dragover", (e) => {
+        e.preventDefault();
+      });
+
+      squareElement.addEventListener("drop", (e) => {
+        e.preventDefault();
+        if(draggedPiece){
+            const targetSquare = {
+                row: parseInt(squareElement.target.dataset.row),
+                col: parseInt(squareElement.target.dataset.col),
+            };
+            
+            handleMove(sourceSquare, targetSquare);
+        }
+      });
+
+      boardElement.appendChild(squareElement);
+    });
+  });
+};
+
+const handleMove = () => {};
+
+const getPieceUnicode = (piece) => {
+  const unicodePieces = {
+    k: "♔",
+    q: "♕",
+    r: "♖",
+    b: "♗",
+    n: "♘",
+    p: "♙",
+    K: "♚",
+    Q: "♛",
+    R: "♜",
+    B: "♝",
+    N: "♞",
+    P: "♟",
+  };
+
+  return unicodePieces[piece.type] || "";
+};
+
+renderBoard();
